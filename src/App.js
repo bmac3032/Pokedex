4cb4db61524829930988a9e7ccc143d796153f50
Brian McPherson <heat3032@gmail.com>
2020-09-07 12:58:33 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import Pokedex from "./Pokedex";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Pokedex />
+      </>
+    );
+  }
+}
+
+export default App;
