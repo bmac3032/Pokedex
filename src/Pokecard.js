0162e916cff24e88218c8350a6b8d2a02a120486
Brian McPherson <heat3032@gmail.com>
2020-09-07 22:30:13 -0400

    Version 2.0
    New styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./Pokecard.css";
-const POKEAPI =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+// const POKEAPI =
+//   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKEAPI = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+let padToThree = (number) =>
+  number <= 999 ? `000${number}`.slice(-3) : number;
 
 class Pokecard extends Component {
   render() {
-    let imgSrc = `${POKEAPI}${this.props.id}.png`;
+    let imgSrc = `${POKEAPI}${padToThree(this.props.id)}.png`;
     return (
       <div className="Pokecard">
         <h1>{this.props.name}</h1>
